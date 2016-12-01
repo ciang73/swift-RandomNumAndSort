554cb1ebd37cf6a1c6c1cff00480562113187645
ciang73 <CK@CKdeMacBook-Air.local>
2016-12-01 11:53:35 +0800

    強加亂數取值方法

--- swift-BaseMethod/ViewController.swift
+++ swift-BaseMethod/ViewController.swift
@@ -26,33 +26,49 @@ class ViewController: UIViewController {
     @IBAction func RandomAction(_ sender: UIButton) {
         rnArr = [Int]()
         rnCount = Int(countOfNumber.text!)!
-        randomFunc(rnCount == 0 ? 10 : rnCount)
+        //randomFunc(rnCount == 0 ? 10 : rnCount)
+        theFastRandomNumber(rnCount == 0 ? 10 : rnCount)
         showArr(label: RandomNumLabel)
     }
-    
-    func randomFunc(_ count: Int){
+    //亂數取值不重覆改良版
+    func theFastRandomNumber(_ count :Int){
+        var array = [count]
+        //首先做出0~count的陣列
         for i in 0...count - 1 {
-            var TF = true   //表示重覆
-            var temp = Int(arc4random_uniform(UInt32(count)))
-            while TF == true && i > 0 {
-                TF = false
-                for j in 0...i - 1 {
-                    if rnArr[j] == temp{
-                        TF = true
-                        temp = Int(arc4random_uniform(UInt32(count)))
-                        break
-                    }
-                }
-            }
-            rnArr.append(temp)
+            array.append(i)
+        }
+        //取值, 刪值
+        while rnArr.count != count {
+            var catchNum = Int(arc4random_uniform(UInt32(array.count)))
+            rnArr.append(array[catchNum])
+            array.remove(at: catchNum)
         }
     }
-
-    @IBAction func SortAction(_ sender: UIButton) {
-        rnArr.count != 0 ? BBSort() : randomFunc(rnCount == 0 ? 10 : rnCount)
-        
-    }
     
+    
+//    func randomFunc(_ count: Int){
+//        for i in 0...count - 1 {
+//            var TF = true   //表示重覆
+//            var temp = Int(arc4random_uniform(UInt32(count)))
+//            while TF == true && i > 0 {
+//                TF = false
+//                for j in 0...i - 1 {
+//                    if rnArr[j] == temp{
+//                        TF = true
+//                        temp = Int(arc4random_uniform(UInt32(count)))
+//                        break
+//                    }
+//                }
+//            }
+//            rnArr.append(temp)
+//        }
+//    }
+    
+    @IBAction func SortAction(_ sender: UIButton) {
+        //rnArr.count != 0 ? BBSort() : randomFunc(rnCount == 0 ? 10 : rnCount)
+        rnArr.count != 0 ? BBSort() : theFastRandomNumber(rnCount == 0 ? 10 : rnCount)
+    }
+    //排序方法
     func BBSort(){
         for i in 0...rnArr.count - 1 {
             for j in 0...i{
@@ -64,7 +80,7 @@ class ViewController: UIViewController {
         }
         showArr(label: SortedNumLabel)
     }
-    
+    //交換方法
     func swap(a: inout Int, b: inout Int){
         var temp:Int
         temp = a
@@ -72,7 +88,7 @@ class ViewController: UIViewController {
         b = temp
     
     }
-    
+    //顯示方法
     func showArr(label: UILabel){
         label.text = ""
         for i in rnArr{
